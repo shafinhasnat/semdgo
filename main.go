ac4fd220471ca9eeaa4dc84ea6ed0c18705897de
Shafin Hasnat <shafinhasnat97@gmail.com>
2025-04-13 17:11:16 +0600

    image file render fixed, default content path changed

--- main.go
+++ main.go
@@ -1,23 +1,16 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
 	"os"
 	"strings"
+	"text/template"
 
 	"github.com/gomarkdown/markdown"
 	"github.com/gomarkdown/markdown/html"
 	"github.com/gomarkdown/markdown/parser"
 )
-
-var mds = `# header
-
-Sample text.
-
-[link](http://example.com)
-`
 
 func mdToHTML(md []byte) []byte {
 	// create markdown parser with extensions
@@ -34,37 +27,41 @@ func mdToHTML(md []byte) []byte {
 }
 
 func main() {
-	// md := []byte(mds)
-	// html := mdToHTML(md)
-
-	// fmt.Printf("--- Markdown:\n%s\n\n--- HTML:\n%s\n", md, html)
 	http.HandleFunc("/", handle)
 	http.ListenAndServe(":80", nil)
 }
 
 func handle(w http.ResponseWriter, r *http.Request) {
 	path := clickedhyperlink(r)
-	log.Printf("path: %s\n", path)
+	log.Printf("Status: %d | path: %s\n", http.StatusOK, path)
 	content, err := os.ReadFile(path)
 	if err != nil {
-		http.Error(w, "Unable to read markdown file", http.StatusInternalServerError)
+		log.Printf("Status: %d | path: %s | Error: %s\n", http.StatusNotFound, path, err)
+		http.Error(w, "Unable to read markdown file", http.StatusNotFound)
+		return
+	}
+	if !strings.HasSuffix(path, ".md") {
+		http.ServeFile(w, r, path)
 		return
 	}
 	html := mdToHTML(content)
-	// Log clicked hyperlinks
-	fmt.Fprintf(w, "%s", html)
+	tmpl, err := template.ParseFiles("./templates/200")
+	if err != nil {
+		panic(err)
+	}
+	tmpl.Execute(w, map[string]interface{}{
+		"Content": string(html),
+	})
+
 }
 
 func clickedhyperlink(r *http.Request) string {
-	basepath := "./content"
+	basepath := "/var/semdgo/content"
 	link := r.URL.Path
-	if !strings.Contains(link, "favicon.ico") {
-		if link == "/" {
-			path := basepath + "/README.md"
-			return path
-		}
-		path := basepath + link
+	if link == "/" {
+		path := basepath + "/README.md"
 		return path
 	}
-	return basepath + "/README.md"
+	path := basepath + link
+	return path
 }
